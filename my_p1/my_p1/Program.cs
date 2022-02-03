d6df0bf3ce12f300813c05fec2eea8a7a3d619b8
Olena <elenapilenaolegovna@gmail.com>
2022-02-03 15:35:39 +0200

    commit1

---
+++ my_p1/my_p1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace my_p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*fkgkgkdgfkjdfklj*/
+        }
+    }
+}
