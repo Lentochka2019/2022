58c80ed1a00863025bbba1c88371e4425d287486
Olena <elenapilenaolegovna@gmail.com>
2022-02-04 13:47:47 +0200

    OperMax

--- my_p1/my_p1/Program.cs
+++ my_p1/my_p1/Program.cs
@@ -6,10 +6,19 @@ namespace my_p1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            /*fkgkgkdgfkjdfklj*/
 
-            /*ророрроо*/
+            // int n1 = 5;
+            //int n2 = 10;
+            Console.Write("Значення1: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Значення2: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+           
+            int maxVar = Math.Max(n1,n2);
+            Console.WriteLine($"Найбiльше значення: {maxVar}");
+            
+
         }
     }
 }
