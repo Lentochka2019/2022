7f54a8d1b4a7f17cccc63af613fe8a4f5f8d023c
Olena <elenapilenaolegovna@gmail.com>
2022-02-05 02:33:49 +0200

    max

--- my_p1/my_p1/Program.cs
+++ my_p1/my_p1/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 
-namespace my_p1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            /*fkgkgkdgfkjdfklj*/
 
+
+namespace my_p1
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                // int n1 = 5;
+                //int n2 = 10;
+                Console.Write("Значення1: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Значення2: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                int maxVar = Math.Max(n1, n2);
+                Console.WriteLine($"Найбiльше значення: {maxVar}");
+
+
+            }
         }
     }
-}
+
