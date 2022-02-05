07a68a4abb1002d9bba099c0dd2d2c8fc43a8904
Olena <elenapilenaolegovna@gmail.com>
2022-02-05 20:38:14 +0200

    Home work Math

--- my_p1/lessondatatype/Program.cs
+++ my_p1/lessondatatype/Program.cs
@@ -6,17 +6,21 @@ namespace lessondatatype
     {
         static void Main(string[] args)
         {
-            Console.Write("Значення1: ");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Значення2: ");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-
+            int n1 = 2;
+            int n2 = 6;
+            Console.WriteLine($"Дано значення: {n1} та {n2}");
             int maxVar = Math.Max(n1, n2);
             Console.WriteLine($"Найбiльше значення: {maxVar}");
             Console.WriteLine($"Рахуємо приклад при значеннi: {n1}");
             var n3 = -6 * Math.Pow(n1, 3) + 5 * Math.Pow(n1, 2) - 10 * n1 + 15;
             Console.WriteLine($"-6*x^3+5*x^2-10*x+15= {n3}");
+            // abs(x) * sin(x) //2 * pi * x
+            double x = 0;
+            Console.WriteLine($"Рахуємо два приклади при значеннi: {x}");
+            x=Math.Abs(x)*Math.Sin(x);
+            Console.WriteLine($"abs(x) * sin(x): {x}");
+            x = 2 * Math.PI * x;
+            Console.WriteLine($"2 * pi * x: {x}");
         }
     }
 }
