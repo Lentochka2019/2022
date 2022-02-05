0f62685c6ff422c96129f1928ad67e510665e3cc
Olena <elenapilenaolegovna@gmail.com>
2022-02-05 02:03:01 +0200

    commit2

--- my_p1/my_p1/Program.cs
+++ my_p1/my_p1/Program.cs
@@ -7,6 +7,7 @@ namespace my_p1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            /*Console.ReadKey();*/
             /*fkgkgkdgfkjdfklj*/
 
             /*ророрроо*/
