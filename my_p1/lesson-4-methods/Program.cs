c90f0866e5049fdafe852ec275dd12cc58238546
Olena <elenapilenaolegovna@gmail.com>
2022-02-09 22:52:18 +0200

    Methods max value and min value

---
+++ my_p1/lesson-4-methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson_4_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Max value:{ValMax(3, 5)}");
+            Console.WriteLine($"Min value:{ValMin(3, 5)}");
+        }
+        static int ValMax(int a, int b)
+        { return Math.Max(a, b); }
+        static int ValMin(int a, int b)
+        { return Math.Min(a, b); }
+    }
+}
