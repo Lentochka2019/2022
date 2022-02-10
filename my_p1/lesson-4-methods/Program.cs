0d05d8dfadf900c89570d278ac98e3059a434a4f
Olena <elenapilenaolegovna@gmail.com>
2022-02-10 14:20:32 +0200

    Method TrySumIfOdd Method Repeat

--- my_p1/lesson-4-methods/Program.cs
+++ my_p1/lesson-4-methods/Program.cs
@@ -7,11 +7,48 @@ namespace lesson_4_methods
         static void Main(string[] args)
         {
             Console.WriteLine($"Max value:{ValMax(3, 5)}");
-            Console.WriteLine($"Min value:{ValMin(3, 5)}");
+            Console.WriteLine($"Min value:{ValMin(3, 5)}");            
+            Console.WriteLine(TrySumIfOdd(5,8,out bool rez));
+            Console.Write("Extra ex: ");
+            Console.WriteLine(Repeat("abc",2,out string str));
+            Console.ReadKey();
         }
         static int ValMax(int a, int b)
         { return Math.Max(a, b); }
         static int ValMin(int a, int b)
         { return Math.Min(a, b); }
+
+
+        /*Завдання 3*/
+       static bool TrySumIfOdd(int c,int d,out bool rezSum)
+        {
+            int sum = c + d;
+            Console.WriteLine($"Sum: {sum}");
+            rezSum = true;
+            if (sum % 2 == 0) return rezSum=true;
+            else return rezSum = false;
+            
+        }
+        /*Завданя 4Рекурсія метод Repeat*/
+        static string Repeat(string X, int N, out string stroka)
+        {
+            if (N == 1)
+            { stroka = X; return X; }
+            else
+            {
+                int i = 0;
+                if (i < N)
+                {
+                    stroka = Repeat(X, N - 1, out stroka);
+                    Console.Write( stroka);
+
+                    
+                }
+                return Repeat(X,1,out stroka);
+            }         
+
+        }
+
+
     }
 }
