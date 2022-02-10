befbcfa19eafe8784e9de82725e0f89b0c83c722
Olena <elenapilenaolegovna@gmail.com>
2022-02-10 20:44:15 +0200

    Мінімальне разраховано через рекурсію

--- my_p1/lesson-4-methods/Program.cs
+++ my_p1/lesson-4-methods/Program.cs
@@ -8,18 +8,21 @@ namespace lesson_4_methods
         {
             Console.WriteLine($"Max value by method 1: {ValMax(3, 5)}");
             ValMax(3,5,7);
+            ValMax(3, 5, 7,6);
             Console.WriteLine($"Min value by method 1: {ValMin(3, 5)}");
-            ValMin(2, 5, 7);
+            Console.WriteLine($"Min value by method 2: {ValMin(10, 5, 7)}");
+            Console.WriteLine($"Min value by method 3: {ValMin(1, 5, 7, 4)}");
             Console.WriteLine(TrySumIfOdd(5,8,out bool rez));
             Console.Write("Extra ex: ");
             Console.WriteLine(Repeat("abc",2,out string str));
             Console.ReadKey();
         }
+        /*Завдання 1 та 2 Визначення максимального та мінімального значення*/
+        /*Максимально знаходжу через Math.Max та цикл*/
         static int ValMax(int a, int b)
-        {           
-            if (a > b) return  a;
-            else return  b;
-             }
+        {
+            return Math.Max(a, b);
+        }
         static void ValMax(params int[] znach)
         {
             int max = znach[0];
@@ -33,20 +36,20 @@ namespace lesson_4_methods
             Console.WriteLine($"Max value by method 2: {max}");
 
         }
+
+        /*Мінімальне через Math.Min та рекурсію*/
         static int ValMin(int a, int b)
         { return Math.Min(a, b); }
-        static void ValMin(params int[] znach)
+        static int ValMin(int a, int b,int c)
+        {
+            return Math.Min(ValMin(a,b),c);
+
+        }
+        static int ValMin(params int[] znach)
         {
             int min = znach[0];
-
-            for (int i = 1; i < znach.Length; i++)
-            {
-
-                if (znach[i] < min)
-                    min = znach[i];
-            }
-            Console.WriteLine($"Min value by method 2: {min}");
-
+            if(znach.Length==1) return znach[0];
+            else return ValMin(min,znach[znach.Length-1]);
         }
 
 
@@ -60,7 +63,7 @@ namespace lesson_4_methods
             else return rezSum = false;
             
         }
-        /*Завданя 4Рекурсія метод Repeat*/
+        /*Завданя 4 Рекурсія метод Repeat*/
         static string Repeat(string X, int N, out string stroka)
         {
             if (N == 1)
