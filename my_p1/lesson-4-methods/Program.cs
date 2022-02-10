3ab176c10c04f2309875ddfbf35bc08f4be4e96e
Olena <elenapilenaolegovna@gmail.com>
2022-02-10 15:28:18 +0200

    Overload first two methods with 3 and 4 parameters

--- my_p1/lesson-4-methods/Program.cs
+++ my_p1/lesson-4-methods/Program.cs
@@ -6,21 +6,52 @@ namespace lesson_4_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Max value:{ValMax(3, 5)}");
-            Console.WriteLine($"Min value:{ValMin(3, 5)}");            
+            Console.WriteLine($"Max value by method 1: {ValMax(3, 5)}");
+            ValMax(3,5,7);
+            Console.WriteLine($"Min value by method 1: {ValMin(3, 5)}");
+            ValMin(2, 5, 7);
             Console.WriteLine(TrySumIfOdd(5,8,out bool rez));
             Console.Write("Extra ex: ");
             Console.WriteLine(Repeat("abc",2,out string str));
             Console.ReadKey();
         }
         static int ValMax(int a, int b)
-        { return Math.Max(a, b); }
+        {           
+            if (a > b) return  a;
+            else return  b;
+             }
+        static void ValMax(params int[] znach)
+        {
+            int max = znach[0];
+            
+            for (int i = 1; i < znach.Length; i++)
+            {
+             
+                if (znach[i] > max)
+                    max = znach[i];
+            }
+            Console.WriteLine($"Max value by method 2: {max}");
+
+        }
         static int ValMin(int a, int b)
         { return Math.Min(a, b); }
+        static void ValMin(params int[] znach)
+        {
+            int min = znach[0];
+
+            for (int i = 1; i < znach.Length; i++)
+            {
+
+                if (znach[i] < min)
+                    min = znach[i];
+            }
+            Console.WriteLine($"Min value by method 2: {min}");
+
+        }
 
 
         /*Завдання 3*/
-       static bool TrySumIfOdd(int c,int d,out bool rezSum)
+        static bool TrySumIfOdd(int c,int d,out bool rezSum)
         {
             int sum = c + d;
             Console.WriteLine($"Sum: {sum}");
