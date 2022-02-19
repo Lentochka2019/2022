9c21536c8781d90d44afdc892a985a5ca1e7ee20
Olena <elenapilenaolegovna@gmail.com>
2022-02-19 21:07:47 +0200

    HOMEWORK 8

---
+++ my_p1/lesson-8-text/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_8_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
